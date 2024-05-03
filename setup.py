ca1cfcbcab068f6aedb2a02b4e63600d6e6df12b
Sumit Yadav <sumityadav329@gmail.com>
2024-05-04 01:15:02 +0530

    added and updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'SumitYadav',
+    author_email= 'sumityadav329@gmail',
+    packages= find_packages(),
+    install_requires = []
+
+)
